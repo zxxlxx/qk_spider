5f200f368c713b7bbddfde3e378b4d4c298bb6b3
licho <chinabhsun@gmail.com>
2016-10-11 10:06:07 +0800

    chose :ignore tmp dir

---
+++ tests/test_user.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+from unittest import TestCase
+
+
+
+class TestUser(TestCase):
+    def test_generate_fake(self):
+        self.fail()
+
+    def test_password(self):
+        self.fail()
+
+    def test_password(self):
+        self.fail()
+
+    def test_verify_password(self):
+        self.fail()
+
+    def test_ping(self):
+        self.fail()
+
+    def test_generate_confirmation_token(self):
+        self.fail()
+
+    def test_confirm(self):
+        self.fail()

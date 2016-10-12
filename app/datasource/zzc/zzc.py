b5f7334458b2f8eb6ad77b44ebe7e0db34bba3de
licho <licho.sun@gmail.com>
2016-10-12 19:17:07 +0800

    feat :完成中智诚数据获取函数,未测

--- app/datasource/zzc/zzc.py
+++ app/datasource/zzc/zzc.py
@@ -31,8 +31,8 @@ BACK_LIST_BASE_URL = NEZHA + "blacklist/"
 
 
 class Zzc:
-    """get zhong zhi cheng anti fraud data"""
-    headers = {'Content-Type': 'application/json'}
+    """获取中智诚反欺诈数据"""
+    headers = {'Content-Type': 'application/json; charset=utf8'}
     auth = (NAME, PASSWORD)
 
     def __init__(self):
@@ -44,7 +44,8 @@ class Zzc:
         result = requests.get(CHEAT_LIST_BASE_URL + apply_id,
                               auth=cls.auth,
                               headers=cls.headers,
-                              timeout=1)
+                              timeout=1,
+                              verify=False)
 
         if result.status_code == requests.codes.ok:
             page = result.json()
@@ -60,7 +61,8 @@ class Zzc:
         result = requests.post(CHEAT_LIST_BASE_URL,
                                json=json_data,
                                auth=cls.auth,
-                               headers=cls.headers)
+                               headers=cls.headers,
+                               verify=False)
         return True if result.status_code == requests.codes.created else False
 
     @classmethod
@@ -70,6 +72,35 @@ class Zzc:
                                json=json_data,
                                auth=cls.auth,
                                headers=cls.headers)
+        return True if result.status_code == requests.codes.ok else False
+
+    @classmethod
+    def delete(cls, apply_id):
+        """删除一条已经上传的申请信息"""
+        result = requests.delete(CHEAT_LIST_BASE_URL + apply_id,
+                                 auth=cls.auth,
+                                 timeout=1,
+                                 verify=False)
+        return True if result.status_code == requests.codes.no_content else False
+
+    @classmethod
+    def anti_fraud_report(cls, apply_id):
+        """该请求获取当前的申请信息的反欺诈报告,包含规则引擎的结果以及黑名单的结果"""
+        result = requests.get(CHEAT_LIST_BASE_URL + apply_id + '/report',
+                              auth=cls.auth,
+                              timeout=30,
+                              verify=False)
+        return True if result.status_code == requests.codes.ok else False
+
+    @classmethod
+    def rule_report(cls, apply_id):
+        """该请求将当前的申请信息提交规则引擎并获取规则执行结果.和反欺诈报告相比,该接口不包含黑名单查询命中的
+        记录的详细信息,但是规则中使用黑名单碰撞的部分仍然可能出现.该接口速度快于反欺诈报告,不需要黑名单命中记录详情的
+        用户可以只调用该接口"""
+        result = requests.get(CHEAT_LIST_BASE_URL + apply_id + "/rule_result",
+                              auth=cls.auth,
+                              timout=10,
+                              verify=False)
         return True if result.status_code == requests.codes.ok else False
 
 if __name__ == '__main__':
--- tests/test_zzc.py
+++ tests/test_zzc.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from unittest import TestCase
 import pytest
 import requests
@@ -5,9 +6,9 @@ import json
 from app.datasource.zzc.zzc import Zzc
 
 
-# -*- coding: utf-8 -*-
 class TestZzc(TestCase):
-    data = {"loan_term":12,"loan_amount":20000,"loan_purpose":"fangdai","loan_type":"12","applicant":{"mobile":"18710723119","name":"wangbing","pid":"610527199005154925"}}
+    data = {"loan_term": 12, "loan_amount": 20000, "loan_purpose": "fangdai", "loan_type": "车货",
+            "applicant": {"mobile": "18710723119", "name": "wangbing", "pid": "610527199005154925"}}
 
     def test_show_by_institution(self):
         result = Zzc.show_by_institution('AP201607179336771654')
@@ -22,3 +23,12 @@ class TestZzc(TestCase):
         json_data = json.dumps(self.data)
         result = Zzc.update('AP201607179336771654', json_data)
         assert result
+
+    def test_delete(self):
+        pass
+
+    def test_anti_fraud_report(self):
+        pass
+
+    def test_rule_report(self):
+        pass

c9220d92068038a5de453c16d5099ecd715fb0f3
licho <licho.sun@gmail.com>
2016-10-16 09:12:55 +0800

    修改过时的包

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.oauthlib.provider import OAuth2Provider
+from flask_oauthlib.provider import OAuth2Provider
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment

e8aebd0ab9a11690ef17710147da7d8b49ed0a06
licho <licho.sun@gmail.com>
2016-10-20 13:53:14 +0800

    refct :zzc更改为非静态.

--- app/datasource/zzc/zzc.py
+++ app/datasource/zzc/zzc.py
@@ -39,13 +39,13 @@ class Zzc:
     def __init__(self):
         pass
 
-    @classmethod
-    def show_by_institution(cls, apply_id):
+    def show_by_institution(self, apply_id):
         """return a apply insformation by specify institution """
         result = requests.get(CHEAT_LIST_BASE_URL + apply_id,
-                              auth=cls.auth,
-                              headers=cls.headers,
+                              auth=self.auth,
+                              headers=self.headers,
                               timeout=1)
+
 
         if result.status_code == requests.codes.ok:
             page = result.json()
@@ -53,49 +53,44 @@ class Zzc:
         else:
             page = ''
             r = False
-        return page, r
+        return page, r 
 
-    @classmethod
-    def create(cls, json_data):
+    def create(self, json_data):
         """create a loan apply information"""
         result = requests.post(CHEAT_LIST_BASE_URL,
                                json=json_data,
-                               auth=cls.auth,
-                               headers=cls.headers)
+                               auth=self.auth,
+                               headers=self.headers)
         return True if result.status_code == requests.codes.created else False
 
-    @classmethod
-    def update(cls, apply_id, json_data):
+    def update(self, apply_id, json_data):
         """update a loan apply information"""
         result = requests.put(CHEAT_LIST_BASE_URL + apply_id,
-                               json=json_data,
-                               auth=cls.auth,
-                               headers=cls.headers)
+                              json=json_data,
+                              auth=self.auth,
+                              headers=self.headers)
         return True if result.status_code == requests.codes.ok else False
 
-    @classmethod
-    def delete(cls, apply_id):
+    def delete(self, apply_id):
         """删除一条已经上传的申请信息"""
         result = requests.delete(CHEAT_LIST_BASE_URL + apply_id,
-                                 auth=cls.auth,
+                                 auth=self.auth,
                                  timeout=1)
         return True if result.status_code == requests.codes.no_content else False
 
-    @classmethod
-    def anti_fraud_report(cls, apply_id):
+    def anti_fraud_report(self, apply_id):
         """该请求获取当前的申请信息的反欺诈报告,包含规则引擎的结果以及黑名单的结果"""
         result = requests.get(CHEAT_LIST_BASE_URL + apply_id + '/report',
-                              auth=cls.auth,
+                              auth=self.auth,
                               timeout=30)
         return True if result.status_code == requests.codes.ok else False
 
-    @classmethod
-    def rule_report(cls, apply_id):
+    def rule_report(self, apply_id):
         """该请求将当前的申请信息提交规则引擎并获取规则执行结果.和反欺诈报告相比,该接口不包含黑名单查询命中的
         记录的详细信息,但是规则中使用黑名单碰撞的部分仍然可能出现.该接口速度快于反欺诈报告,不需要黑名单命中记录详情的
         用户可以只调用该接口"""
         result = requests.get(CHEAT_LIST_BASE_URL + apply_id + "/rule_result",
-                              auth=cls.auth,
+                              auth=self.auth,
                               timeout=10)
         return True if result.status_code == requests.codes.ok else False
 

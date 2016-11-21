1285a2536d604a09f556a31e978796f0d7bd297e
licho <chinabhsun@gmail.com>
2016-11-21 16:08:20 +0800

    添加钱海验证

--- app/datasource/qianhai/dataSecurityUtil.py
+++ app/datasource/qianhai/dataSecurityUtil.py
@@ -1,21 +1,67 @@
 # -*- coding: utf-8 -*-
 import base64
+import os
 
-from cryptography.hazmat.primitives import padding
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import padding as primitives_padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding as asymmetric_padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+import jks
+import textwrap
+
 
 class DataSecurityUtil:
-
-
     def digest(self, origin_byte):
         pass
 
-    def getPublicKey(self):
+    def get_public_key(self):
         pass
 
-    def get_private_key(self):
-        pass
+    @staticmethod
+    def print_pem(der_bytes, type):
+        print("-----BEGIN %s-----" % type)
+        print("\r\n".join(textwrap.wrap(base64.b64encode(der_bytes).decode('ascii'), 64)))
+        print("-----END %s-----" % type)
+
+    @staticmethod
+    def get_pem(der_bytes, type):
+        result = "-----BEGIN {type}-----\r\n" \
+                 "{data}\r\n" \
+                 "-----END {type}-----".format(type=type,
+                                               data="\r\n".join(textwrap.wrap(
+                                                   base64.b64encode(der_bytes).decode('ascii'), 64)))
+        print(result)
+        return result
+
+    @staticmethod
+    def get_private_key(file_path=None):
+        """
+        获取jks后缀的私钥
+        :return:
+        """
+        if not file_path:
+            file_name = os.path.abspath(os.path.dirname(__file__)) + '/credoo_stg.jks'
+        ks = jks.KeyStore.load(file_name, 'qhzx_stg')
+        pk = ks.private_keys.get('signkey')
+
+        if pk.algorithm_oid == jks.util.RSA_ENCRYPTION_OID:
+            p_key = DataSecurityUtil.get_pem(pk.pkey, "RSA PRIVATE KEY")
+
+        private_key = serialization.load_pem_private_key(p_key.encode(), password=None, backend=default_backend())
+
+        certificate = DataSecurityUtil.get_pem(pk.cert_chain[0][1], "CERTIFICATE")
+        cert = x509.load_pem_x509_certificate(certificate.encode(), default_backend())
+        return private_key, cert
+
+    @staticmethod
+    def sign_data(data):
+        private_key, _ = DataSecurityUtil.get_private_key()
+        sign = private_key.sign(data, asymmetric_padding.PKCS1v15(), hashes.SHA1())
+        result = base64.b64encode(sign)
+        return result
 
     @staticmethod
     def encrypt(origin, key):
@@ -27,7 +73,7 @@ class DataSecurityUtil:
         """
         cipher = Cipher(algorithms.TripleDES(key), modes.ECB(), default_backend())
         encryptor = cipher.encryptor()
-        padder = padding.PKCS7(64).padder()
+        padder = primitives_padding.PKCS7(64).padder()
         padder_data = padder.update(origin) + padder.finalize()
         print(len(padder_data))
         ct = encryptor.update(padder_data) + encryptor.finalize()
@@ -47,7 +93,6 @@ class DataSecurityUtil:
         cipher = Cipher(algorithms.TripleDES(key), modes.ECB(), default_backend())
         decryptor = cipher.decryptor()
         origin = decryptor.update(data) + decryptor.finalize()
-        unpadder = padding.PKCS7(64).unpadder()
+        unpadder = primitives_padding.PKCS7(64).unpadder()
         origin_result = unpadder.update(origin) + unpadder.finalize()
         return origin_result
-
--- tests/test_dataSecurityUtil.py
+++ tests/test_dataSecurityUtil.py
@@ -1,7 +1,6 @@
 import base64
 from unittest import TestCase
 
-
 # -*- coding: utf-8 -*-
 from app.datasource.qianhai.dataSecurityUtil import DataSecurityUtil
 
@@ -12,16 +11,34 @@ class TestDataSecurityUtil(TestCase):
     def test_digest(self):
         self.fail()
 
+    def test_sign_data(self):
+        a = 'a'
+        result = DataSecurityUtil.sign_data(a.encode())
+        assert a == 'AlVsreY14EX3UwWlRms1R6NrX2N1XMKQMqdfkiz5eZihn5lnSbkufxFJjxencEyYXhmJGkUy' \
+                    'gUS8tI+oHwzXZBEY36SdhsE1R7PSgAuew2DAX2nQDcqPd+XVt3AW2c5PHpM0PJQ' \
+                    'o8xB6mh/Cx6PsCyzFxs1pFwaU72aMfutmaQ8='
+
     def test_getPublicKey(self):
         self.fail()
 
+    def test_get_private_key(self):
+        DataSecurityUtil.get_private_key()
+
     def test_encrypt(self):
-        message = "Fuchk QianHai".encode()
+        message = "q3[945eorigjpeqtjg;oaitpq3tj;kjpq0ruq[345rijf".encode()
         encry = DataSecurityUtil.encrypt(message, TestDataSecurityUtil.key)
         print(encry)
 
     def test_decrypt(self):
-        abc = "eC3XXlsQd4RJD5XN3vcKNg==".encode()
+        abc = "NALzh0JM3QTkZj2QD9+zAxtwnlQHLXBo0UvkTOl+JIyAyBVeQKj6zH5jxpeTRRlD".encode()
         message = DataSecurityUtil.decrypt(abc, TestDataSecurityUtil.key)
 
         print(message.decode())
+
+
+if __name__ == '__main__':
+    # busi_data = '{"batchNo": "33adfsf323233", "records": [{"reasonCode": "01", "idNo": "362528198745421654", "idType": "0", "name": "唐唐", "seqNo": "r231545334545"}]}'
+    busi_data = 'nimadeqianhaifuckqianhaiNMDCNMDQIANHAILAJIBITCHTHISisenoughlong'
+    enc_busi_data = DataSecurityUtil.encrypt(busi_data.encode(), TestDataSecurityUtil.key)
+    sig_value = DataSecurityUtil.sign_data(enc_busi_data)
+    print(sig_value.decode())
